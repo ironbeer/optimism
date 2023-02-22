b90a5d5f6e49e8d7e6c2230a7744097eacf41a15
Mark Tyneway <mark.tyneway@gmail.com>
2023-02-21 19:29:09 -0800

    contracts-bedrock: cleanup fixes

--- packages/contracts-bedrock/contracts/test/CommonTest.t.sol
+++ packages/contracts-bedrock/contracts/test/CommonTest.t.sol
@@ -223,8 +223,6 @@ contract Messenger_Initializer is L2OutputOracle_Initializer {
             _paused: false
         });
         vm.label(address(op), "OptimismPortal");
-        vm.prank(op.GUARDIAN());
-        op.unpause();
 
         // Deploy the address manager
         vm.prank(multisig);
